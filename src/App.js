a8f149d27070143334f75965f7cd7872ef121e90
Dewan Mohammad Tasinuzzaman <dewanmohammad.tasinuzzaman@ontariotechu.net>
2023-10-01 03:01:58 -0400

    Created the components directory structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/views/Home';
+import Nav from './components/navigation/Nav';
+
 
 function App() {
   return (
     <div className="App">
-      
+
+
+      {/* Different Pages Displayed based on Routes */}
+      <Home />
+
+
+
+      {/* Bottom Nav Bar - Fixed - Stays on Every Page */}
+      <Nav />
+
+
     </div>
   );
 }
---
+++ src/components/views/Home.jsx
@@ -0,0 +1,14 @@
+// Home.jsx
+
+import React from 'react';
+import '../../styles/main.scss';
+
+function Home() {
+  return (
+    <div>
+        HELLO
+    </div>
+  );
+}
+
+export default Home;
